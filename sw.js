111e623ca2a42d7acd036b82988605fffab48812
himanshudubey1244 <himanshudubey1244@gmail.com>
2026-06-12 11:06:51 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-var CACHE='airdraw-v1';
-var ASSETS=['./index.html','./manifest.json'];
-self.addEventListener('install',function(e){
-  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}));
-});
-self.addEventListener('fetch',function(e){
-  e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request);}));
-});
